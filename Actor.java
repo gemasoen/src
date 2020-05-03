542b6ccc6ebc5c197cd8ea3dd79343841f4cc3a7
gemasoen <64735837+gemasoen@users.noreply.github.com>
2020-05-03 15:30:51 +0200

    Add files via upload

---
+++ Actor.java
@@ -0,0 +1,7 @@
+// Fitxer Actor.java
+package episodis;
+
+public class Director {
+	private int codi;
+	private String nom;
+}
---
+++ Director.java
@@ -0,0 +1,8 @@
+// Fitxer Director.java
+package episodis;
+
+public class Actor {
+	private int codi;
+	private String nom;
+	private String personatge;
+}
